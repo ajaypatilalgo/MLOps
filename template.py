33eed9e1a0629a5bbb50f2301bf61412b6cff342
ajaypatilalgo <ajay.patil@algorisys.com>
2024-11-28 12:41:37 +0530

    New updates

--- template.py
+++ template.py
@@ -2,42 +2,50 @@ import os
 from pathlib import Path
 import logging
 
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "textSummarizer"
+
 list_of_files = [
-    '.github/workflows/.gitkeep',
-    'src/__init__.py',
-    'src/components/__init__.py',
-    'src/components/data_ingetion.py',
-    'src/components/data_transformation.py',
-    'src/components/model_trainer.py',
-    'src/components/model_evaluation.py',
-    'src/pipeline/__init__.py',
-    'src/pipeline/training_pipeline.py',
-    'src/pipeline/prediction_pipeline.py',
-    'src/logger/logging.py',
-    'src/exception/exception.py',
-    'src/utils/__init__.py',
-    'src/utils/utils.py',
-    'tests/unit/__init__.py',
-    'tests/integration/__init__.py',
-    'init_setup.sh',
-    'requirements.txt',
-    'requirements_dev.txt',
-    'setup.py',
-    'setup.cfg',
-    'pyproject.toml',
-    'tox.ini',
-    'experiment/experiments.ipynb',
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
 
 ]
 
+
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir , filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)
 
-    if filedir != '':
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory : {filedir} for file : {filename}")
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
     
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
